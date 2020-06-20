ab489b6824778a99b66c0b41a66c2590ee529b79
hika, maeng <hika00@gmail.com>
2020-06-21 02:26:51 +0900

    Create by.js

---
+++ by.js
@@ -0,0 +1,17 @@
+class by{
+  static getValue = Symbol();
+  static setValue = Symbol();
+  static set(cls){
+    Object.entries(cls).forEach(([k, dele])=>{
+      if(typeof dele != 'function' ||
+         typeof dele.prototype[by.getValue] != 'function' ||
+         typeof dele.prototype[by.setValue] != 'function') return;
+      delete cls[k];
+      const delegate = new dele;
+      Object.defineProperty(cls.prototype, k, {
+        get(){return delegate[by.getValue](this, k);},
+        set(v){delegate[by.setValue](this, k, v);}
+      });
+    });
+  }
+}
