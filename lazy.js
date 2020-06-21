4f790260bb8d446a3b6bb35a3d9838b62a5b3541
hika, maeng <hika00@gmail.com>
2020-06-21 13:17:35 +0900

    Create lazy.js

---
+++ lazy.js
@@ -0,0 +1,18 @@
+const lazy =(_=>{
+  class Lazy{
+    static #EMPTY = Symbol();
+    #f;
+    #v = Lazy.#EMPTY;
+
+    constructor(f){
+      this.#f = f;
+    }
+
+    [by.getValue](target, k){
+      if(this.#v === Lazy.#EMPTY) this.#v = this.#f(target);
+      return this.#v;
+    }
+    [by.setValue](target, k, v){throw "read only";}
+  }
+  return f=>new Lazy(f);
+})();
